2c3f555b3786357d6907225f984748b38a4c5ee3
Charlotte LECONTE <charlotte.leconte91@gmail.com>
2020-01-02 16:59:20 +0100

    creation of folder test

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -1,14 +1,6 @@
-const assert = require('assert');
-
-// Check that capitalizeFirst transforms javaScript correctly
-assert.strictEqual(capitalizeFirstLetters('i am learning TDD'), 'I Am Learning TDD');
-// Check that it works for a 1-character string
-assert.strictEqual(capitalizeFirstLetters('zazou'), 'Zazou');
-// Check that it works for an empty string
-assert.strictEqual(capitalizeFirstLetters(''), '');
 
 const capitalizeFirstLetters = (input) => {
   return input.length > 0
-    ? input.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ')
-    : '';   
+    ? input[0].toUpperCase() + input.slice(1)
+    : ''; 
 }
---
+++ test/capitalizeFirstLetters-tests.js
@@ -0,0 +1,9 @@
+const assert = require('assert');
+const capitalizeFirstLetters = require('../capitalizeFirstLetters.js')
+
+// Check that capitalizeFirst transforms javaScript correctly
+assert.strictEqual(capitalizeFirstLetters('i am learning TDD'), 'I Am Learning TDD');
+// Check that it works for a 1-character string
+assert.strictEqual(capitalizeFirstLetters('zazou'), 'Zazou');
+// Check that it works for an empty string
+assert.strictEqual(capitalizeFirstLetters(''), '');
